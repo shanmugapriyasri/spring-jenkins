39ae95d3a504be76d4bdc28d420863c513e9d458
shanmugapriyasri <shanmugi.shankar@gmail.com>
2022-04-12 09:45:47 +0530

    first-commit

---
+++ src/main/java/com/cognizant/HelloController.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String hi() {
+		return "index";
+	}
+}
